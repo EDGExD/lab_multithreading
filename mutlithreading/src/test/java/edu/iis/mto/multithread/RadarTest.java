36d34978cb4ef27f36f9249f0df6bba524ce4dc2
Dominik Snopek <222572@edu.p.lodz.pl>
2021-04-22 19:02:08 +0200

    Update Test launchPatriotOnceWhenNoticesAScudMissle

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,11 +1,14 @@
 package edu.iis.mto.multithread;
 
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.ExecutorService;
 
 @ExtendWith(MockitoExtension.class)
 class RadarTest {
@@ -13,12 +16,23 @@ class RadarTest {
     @Mock
     private PatriotBattery batteryMock;
 
+    @Mock
+    private ExecutorService executorService;
+
     @Test
     void launchPatriotOnceWhenNoticesAScudMissle() {
-        Radar radar = new Radar(batteryMock);
+
+        when(executorService.submit(any(Runnable.class))).thenAnswer(invocationOnMock -> {
+            ((Runnable) invocationOnMock.getArgument(0)).run();
+            return null;
+        });
+
+        BetterRadar radar = new BetterRadar(1,batteryMock,executorService);
         Scud enemyMissle = new Scud();
         radar.notice(enemyMissle);
+
         verify(batteryMock).launchPatriot(enemyMissle);
+
     }
 
 }
