e270f3a22133e34401f9043d380a7afcbe55f23d
Jakub Wlaźlak <222611@edu.p.lodz.pl>
2021-04-23 12:00:57 +0200

    Dodanie testu sprawdzającego poprawne wysłanie rakiety w przypadku jednej rakiety wroga

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,11 +1,15 @@
 package edu.iis.mto.multithread;
 
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.Executor;
 
 @ExtendWith(MockitoExtension.class)
 class RadarTest {
@@ -13,12 +17,14 @@ class RadarTest {
     @Mock
     private PatriotBattery batteryMock;
 
-    @Test
+    @RepeatedTest(50)
     void launchPatriotOnceWhenNoticesAScudMissle() {
-        Radar radar = new Radar(batteryMock);
+        Executor executor = Runnable::run;
+        final int numberOfRockets = 1;
+        BetterRadar radar = new BetterRadar(executor,numberOfRockets,batteryMock);
         Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle);
-        verify(batteryMock).launchPatriot(enemyMissle);
+        radar.onNotice(enemyMissle);
+        verify(batteryMock,times(numberOfRockets)).launchPatriot(enemyMissle);
     }
 
 }
