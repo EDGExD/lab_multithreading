82256aff91dca6739fc45150f2af954dcb0d0c98
Muras.Wojciech <215828@edu.p.lodz.pl>
2021-04-23 16:28:53 +0200

    Fix and implement test for BetterRadar

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,8 +1,10 @@
 package edu.iis.mto.multithread;
 
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -12,13 +14,32 @@ class RadarTest {
 
     @Mock
     private PatriotBattery batteryMock;
+    @Mock
+    TaskService taskService;
 
-    @Test
+    @BeforeEach
+    void init(){
+        doAnswer(call -> {
+            Runnable runnable = call.getArgument(0);
+            runnable.run();
+            return null;
+        }).when(taskService).getTask(any());
+    }
+
+    @RepeatedTest(20)
     void launchPatriotOnceWhenNoticesAScudMissle() {
-        Radar radar = new Radar(batteryMock);
+        BetterRadar radar = new BetterRadar(batteryMock, taskService, 1);
         Scud enemyMissle = new Scud();
         radar.notice(enemyMissle);
         verify(batteryMock).launchPatriot(enemyMissle);
+    }
+
+    @RepeatedTest(20)
+    public void launchPatriotFiveTimesWhenNoticesFiveScudMissle() {
+        BetterRadar betterRadar = new BetterRadar(batteryMock, taskService, 5);
+        Scud enemyMissle = new Scud();
+        betterRadar.notice(enemyMissle);
+        verify(batteryMock, times(5)).launchPatriot(enemyMissle);
     }
 
 }
