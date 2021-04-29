f7b22bc3983bce86448d4286581b1f4750fec792
EDGExD <222585@edu.p.lodz.pl>
2021-04-29 13:28:50 +0200

    Dodanie kilku testów klasy BetterRadar.

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,10 +1,14 @@
 package edu.iis.mto.multithread;
 
+import java.util.concurrent.ExecutorService;
+import org.junit.jupiter.api.RepeatedTest;
 import static org.mockito.Mockito.verify;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
 import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -13,12 +17,36 @@ class RadarTest {
     @Mock
     private PatriotBattery batteryMock;
 
-    @Test
+    @Mock
+    private ExecutorService executor;
+    
+    @RepeatedTest(30)
     void launchPatriotOnceWhenNoticesAScudMissle() {
-        Radar radar = new Radar(batteryMock);
+        when(executor.submit(any(Runnable.class))).thenAnswer(invocation -> {((Runnable) invocation.getArgument(0)).run();return null;});
+        
+        BetterRadar betterRadar = new BetterRadar(executor, 1, batteryMock);
         Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle);
-        verify(batteryMock).launchPatriot(enemyMissle);
+        betterRadar.notice(enemyMissle);
+        verify(batteryMock, times(1)).launchPatriot(enemyMissle);
     }
 
+    @RepeatedTest(30)
+    void launchPatriotFiveTimesWhenNoticesAScudMissle() {
+        when(executor.submit(any(Runnable.class))).thenAnswer(invocation -> {((Runnable) invocation.getArgument(0)).run();return null;});
+        
+        BetterRadar betterRadar = new BetterRadar(executor, 5, batteryMock);
+        Scud enemyMissle = new Scud();
+        betterRadar.notice(enemyMissle);
+        verify(batteryMock, times(5)).launchPatriot(enemyMissle);
+    }
+    
+    @RepeatedTest(30)
+    void noLaunchPatriot() {
+        when(executor.submit(any(Runnable.class))).thenAnswer(invocation -> {((Runnable) invocation.getArgument(0)).run();return null;});
+        
+        BetterRadar betterRadar = new BetterRadar(executor, 0, batteryMock);
+        Scud enemyMissle = new Scud();
+        betterRadar.notice(enemyMissle);
+        verify(batteryMock, times(0)).launchPatriot(enemyMissle);
+    }
 }
